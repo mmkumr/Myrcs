1066dee48318778361b70edd056b8c1b452d4536
Mukesh Kumar <mmkumr.ping@gmail.com>
2019-03-17 17:02:41 +0530

    Auto commit

--- recoverrc.sh
+++ recoverrc.sh
@@ -2,3 +2,4 @@ cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.zshrc ~/
 cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/i3 ~/.config/ -r
 cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/i3status ~/.config/ -r
 cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/nvim ~/.config/ -r
+cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.muttrc ~/
