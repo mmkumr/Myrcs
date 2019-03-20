93fb3122721f2d5a3b29dfc32c3b9f7e07dbaf2d
M. Mukesh Kumar <mmkumr.ping@gmail.com>
2019-03-20 22:40:10 +0530

    Modified recoverrc.sh

--- recoverrc.sh
+++ recoverrc.sh
@@ -1,5 +1,3 @@
 cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.zshrc ~/
-cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/i3 ~/.config/ -r
-cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/i3status ~/.config/ -r
-cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/nvim ~/.config/ -r
 cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.muttrc ~/
+cp /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/* ~/.config/ -r
