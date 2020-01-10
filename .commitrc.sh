c13e542f28260b83e6e8c6f5929721775e00741c
Mukesh Kumar <mmkumr.ping@gmail.com>
2020-01-10 22:01:51 +0530

    Auto commit

---
+++ .commitrc.sh
@@ -0,0 +1,29 @@
+#Copying rc file to git directory.
+cp ~/.commitrc.sh /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/
+cp ~/.zshrc /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/
+cp ~/.config/i3/ /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/ -r
+cp ~/.config/xfce4 /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/ -r
+cp ~/.config/nvim/ /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/ -r
+cp ~/.config/ranger /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/ -r
+cp ~/.config/termite /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/ -r
+cp ~/.config/sxhkd /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/.config/ -r
+#Changing to git directory.
+cd /media/mmkumr/MyWorkspace/Code/GitHub/Myrcs/
+git status 
+git status | grep clean
+if [ $? -gt 0 ] 
+then
+    echo -n "Do you want to commit rc files? [Y/n] "
+    read REPLY
+    if test "$REPLY" = "N" -o "$REPLY" = "n"; then
+    echo "Operation canceled"
+    sleep 2
+    else
+        git add .
+        git commit -m "Auto commit"
+        git push
+    fi
+fi
+
+#netstat -natu | grep 'ESTABLISHED'
+
