b9c4ec2b7e2725409d39d721ddc15de0795c97c4
Mukesh Kumar <mmkumr.ping@gmail.com>
2024-03-08 01:35:33 +0530

    Auto commit

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -24,7 +24,7 @@ return {
                 expand = function(args)
                     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
                     --require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    vim.fn["UltiSnips#Anon"](args.body)  -- For `ultisnips` users.
                 end,
             },
             window = {
@@ -49,9 +49,9 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
-                { name = 'vsnip' }, -- For vsnip users.
+                { name = 'vsnip' },     -- For vsnip users.
                 --{ name = 'luasnip' }, -- For luasnip users.
-                --{ name = 'ultisnips' }, -- For ultisnips users.
+                { name = 'ultisnips' }, -- For ultisnips users.
                 -- { name = 'snippy' }, -- For snippy users.
             }, {
                 { name = "codeium" },
