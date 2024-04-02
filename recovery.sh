e2cfae87a7e9e44b4141e9ab1ed6258be9ab9c35
Mukesh Kumar <mmkumr.ping@gmail.com>
2024-04-02 23:41:22 +0530

    Auto commit

---
+++ recovery.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+cp  .commitrc.sh .i3status.conf .i3/ .muttrc .zshrc .zsh_history ~/ -r 
+
+cp .config/* ~/.config/ -r
+
+sudo cp environment /etc/environment
+cp g  ~/.local/share/ -r
