ab313021583ea829182e2a92ee5bb6b18db67762
Mukesh Kumar <mmkumr.ping@gmail.com>
2025-02-22 15:26:40 +0530

    Auto commit

--- home/.config/nvim/keybind.lua
+++ home/.config/nvim/keybind.lua
@@ -17,6 +17,7 @@ vim.cmd "nmap vs :vsplit<cr>"
 vim.cmd "nmap hs :split<cr>"
 -- Mapping for clearing highlighted search word
 vim.cmd "nnoremap <tab> :BufferLineCycleNext<CR>"
+vim.cmd "nnoremap <S-tab> :BufferLineCyclePrev<CR>"
 vim.cmd "map <esc> :noh<cr>"
 vim.cmd "map T :terminal<CR>"
 vim.cmd "nnoremap L :Lazy<CR>"
