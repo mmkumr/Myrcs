d385b994e5ffc4391861b6dfe9aadc5aecdead7a
Mukesh Kumar <mmkumr.ping@gmail.com>
2025-07-30 16:08:12 +0530

    Auto commit

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 # Installing packages
-sudo pacman -S pipewire pipewire-alsa pipewire-pulse a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore nautilus gnome-disk-utility slim sxhkd gparted slim-themes alacritty neovim python-pip php  zathura yay xfce4-clipman-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-dev-tools xfce4-mount-plugin xfce4-notifyd xfce4-netload-plugin xfce4-mount-plugin xfce4-power-manager xfce4-whiskermenu-plugin rofi  zathura-pdf-poppler ntfs-3g transmission-gtk npm thunderbird xarchiver unzip tar unrar lf bluez bluez-utils bluez-tools stow libinput-gestures zoxide docker tmux
+sudo pacman -S pipewire pipewire-alsa pipewire-pulse a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore nautilus gnome-disk-utility slim sxhkd gparted slim-themes alacritty neovim python-pip php  zathura yay xfce4-clipman-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-dev-tools xfce4-mount-plugin xfce4-notifyd xfce4-netload-plugin xfce4-mount-plugin xfce4-power-manager xfce4-whiskermenu-plugin rofi  zathura-pdf-poppler ntfs-3g transmission-gtk npm thunderbird xarchiver unzip tar unrar lf bluez bluez-utils bluez-tools stow libinput-gestures zoxide docker tmux screen
+
+# installing Mpv separately for avoid ffmpeg plugin
+sudo pacman -Syu mpv
 
 # Installing AUR packages
-yay -S i3lock-color onlyoffice-desktopeditors-git wps-office wps-office-fonts simple-scan obs-studio android-studio opera blueman flameshot ttf-nerd-fonts-symbols ttf-meslo-nerd ttf-font-awesome ttf-ubuntu-nerd rofi-emoji rofi-power-menu rofi-greenclip arduino-bin libtiff5 epson-inkjet-printer-201601w snapd onlyoffice-desktopeditors nomacs warpinator mpv-full
+yay -S i3lock-color onlyoffice-desktopeditors-git wps-office wps-office-fonts simple-scan obs-studio opera blueman flameshot ttf-nerd-fonts-symbols ttf-meslo-nerd ttf-font-awesome ttf-ubuntu-nerd rofi-emoji rofi-power-menu rofi-greenclip arduino-bin libtiff5 epson-inkjet-printer-201601w snapd onlyoffice-desktopeditors nomacs warpinator 
 # For avoiding rust to failing.
 sudo pacman -S rustup
 rustup install stable
@@ -16,14 +19,23 @@ curl -O https://codeberg.org/tplasdio/g.sh/raw/branch/master/packaging/PKGBUILD-
 pip install --break-system-packages neovim
 # End
 
-# Installing flutter
-sudo mkdir /usr/lib/flutter/
-sudo wget -qO- https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.6-stable.tar.xz | \
-    tar -xvzf -C /usr/lib/ \
-    sudo chown mmkumr:mmkumr /usr/lib/flutter/ -R \
-    flutter upgrade
+# Installing Android Studio
+sudo mkdir -p /opt/android-studio && \
+    curl -L https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.2.1.20/android-studio-2023.2.1.20-linux.tar.gz | \
+    sudo tar -xz -C /opt/android-studio --strip-components=1
 # End
 
+# Installing flutter
+sudo mkdir -p /usr/lib/flutter/ && \
+    curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.8-stable.tar.xz | \
+    sudo tar -xJ -C /usr/lib/flutter --strip-components=1 && \
+    sudo chown -R mmkumr:mmkumr /usr/lib/flutter/ && \
+    flutter upgrade && \
+    sudo pacman -S jdk17-openjdk && \
+    flutter config --jdk-dir=/usr/lib/jvm/java-17-openjdk && \
+    flutter config --android-studio-dir=/opt/android-studio && \
+    flutter doctor -v
+# End
 # Making scripts executable.
 sudo chmod +x ~/.i3/scripts/ -R
 sudo chmod +x ~/.commands/ -R
@@ -45,4 +57,5 @@ systemctl start docker.service
 sudo usermod -aG docker $USER
 
 chsh -s /bin/zsh
+setxkbmap -option caps:swapescape
 # End
